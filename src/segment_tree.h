120cf2d36c47cfb685cee2d51850f103a70fb01d
fiffeek <fm394471@students.mimuw.edu.pl>
2019-01-17 23:59:20 +0100

    Update const correctness, add interface for segment trees, update class hierarchy and arguments visibility

--- src/segment_tree.h
+++ src/segment_tree.h
@@ -9,6 +9,22 @@
 namespace tree {
     using ul = unsigned long; // tree's indexes are represented as an unsigned long data_type for simplicity
 
+    template<typename data_type>
+    using tree_builder_fn = std::function<data_type (const data_type& left, const data_type& right)>;
+
+    template<typename data_type>
+    using query_fn = tree_builder_fn<data_type>;
+
+    template<typename data_type>
+    class segment_tree_interface {
+    public:
+        virtual void leaf_update(ul leaf_index, data_type updater, query_fn<data_type> result_function) = 0;
+        virtual data_type iterative_query(ul s_index, ul e_index, query_fn<data_type> result_function) = 0;
+        virtual data_type get_leaf_value(ul index) const = 0;
+        virtual data_type get_root_value() const = 0;
+        virtual void print_level_by_level() const = 0;
+    };
+
     /**
      * A segment tree class.
      * @tparam data_type a type of elements stored in a specific tree,
@@ -17,14 +33,15 @@ namespace tree {
      * It should be expressed explicitly by concepts when they come in C++20.
      */
     template<typename data_type>
-    class segment_tree {
+    class segment_tree
+            : segment_tree_interface<data_type> {
         static_assert(std::is_copy_assignable<data_type>::value && std::is_copy_constructible<data_type>::value);
-        using tree_builder_fn = std::function<data_type (const data_type& left, const data_type& right)>;
-        using query_fn = tree_builder_fn;
 
     public:
-        segment_tree(ul desirable_size, data_type default_value, tree_builder_fn default_function)
-                : dflt(default_value), fn(default_function) {
+        segment_tree(const ul desirable_size,
+                     const data_type default_value,
+                     const tree_builder_fn<data_type> default_function)
+                : default_argument(default_value), default_function(default_function) {
             if (desirable_size < 1) {
                 throw std::out_of_range("Size of a tree should exceed 1.");
             }
@@ -44,7 +61,9 @@ namespace tree {
          * @param result_function tells the function how to combine the previous
          * value in a leaf with @updater
          */
-        virtual void leaf_update(ul leaf_index, data_type updater, query_fn result_function) {
+        virtual void leaf_update(ul leaf_index,
+                                 const data_type updater,
+                                 const query_fn<data_type> result_function) {
             is_in_leaf_bounds(leaf_index);
 
             leaf_index += size; // leafs in a tree are stored at (size, ... , 2 * size - 1) positions
@@ -52,7 +71,7 @@ namespace tree {
             leaf_index >>= 1;
 
             while (leaf_index) {
-                tree[leaf_index] = fn(tree[left_child(leaf_index)], tree[right_child(leaf_index)]);
+                tree[leaf_index] = default_function(tree[left_child(leaf_index)], tree[right_child(leaf_index)]);
                 leaf_index >>= 1;
             }
         }
@@ -60,8 +79,8 @@ namespace tree {
         /**
          * Provides a default function to update a leaf that uses @fn
          */
-        virtual void leaf_update(ul leaf_index, data_type updater) {
-            leaf_update(leaf_index, updater, fn);
+        virtual void leaf_update(ul leaf_index, const data_type updater) {
+            leaf_update(leaf_index, updater, default_function);
         }
 
         /**
@@ -71,13 +90,13 @@ namespace tree {
          * @param e_index an end index for the resulting interval
          * @param result_function function to accumulate the result into, the left argument is an accumulator
          */
-        virtual data_type iterative_query(ul s_index, ul e_index, query_fn result_function) {
+        virtual data_type iterative_query(ul s_index, ul e_index, const query_fn<data_type> result_function) {
             if (s_index > e_index) {
                 throw std::out_of_range("Indexes are overlapping, a start is greater than the end.");
             }
 
             s_index += size; e_index += size;
-            data_type result = dflt;
+            data_type result = default_argument;
 
             result = result_function(result, tree[s_index]);
             // if start index is the same as end index there is no need to do anything with the result
@@ -108,15 +127,15 @@ namespace tree {
         }
 
         /* Query with a result_function set to the default one which is defined in a class. */
-        virtual data_type iterative_query(ul s_index, ul e_index) {
-            return iterative_query(s_index, e_index, fn);
+        virtual data_type iterative_query(ul start_index, ul end_index) {
+            return iterative_query(start_index, end_index, default_function);
         }
 
-        void print_leafs() {
+        void print_leafs() const {
             print_range(0, size);
         }
 
-        void print_level_by_level() {
+        virtual void print_level_by_level() const {
             ul level_size = 1;
 
             while (level_size <= size) {
@@ -125,49 +144,50 @@ namespace tree {
             }
         }
 
-        data_type get_leaf_value(ul index) {
+        virtual data_type get_leaf_value(const ul index) const {
             is_in_leaf_bounds(index);
 
             return get_node_value(index);
         }
 
-        data_type get_root_value() {
+        virtual data_type get_root_value() const {
             return get_node_value(1);
+        }
+
+        virtual data_type get_node_value(const ul index) const {
+            return tree[index];
         }
 
         virtual ~segment_tree() = default;
 
     protected:
-        void print_range(ul start, ul end) {
+        data_type default_argument;
+        ul size = 1;
+        std::vector<data_type> tree; // for convenience purposes a root is stores at index 1
+        tree_builder_fn<data_type> default_function;
+
+    private:
+        ul left_child(const ul node) const noexcept {
+            return 2 * node;
+        }
+
+        ul right_child(const ul node) const noexcept {
+            return 2 * node + 1;
+        }
+
+        void is_in_leaf_bounds(const ul leaf_index) const {
+            if (!(0 <= leaf_index && leaf_index < size)) {
+                throw std::out_of_range("Provided index is out of range.");
+            }
+        }
+
+        void print_range(const ul start, const ul end) const {
             for (ul index = start; index < end; ++index) {
                 std::cout << tree[index + end] << " ";
             }
 
             std::cout << std::endl;
         }
-
-        ul left_child(ul node) noexcept {
-            return 2 * node;
-        }
-
-        ul right_child(ul node) noexcept {
-            return 2 * node + 1;
-        }
-
-        data_type get_node_value(ul index) noexcept {
-            return tree[index];
-        }
-
-        void is_in_leaf_bounds(ul leaf_index) {
-            if (!(0 <= leaf_index && leaf_index < size)) {
-                throw std::out_of_range("Provided index is out of range.");
-            }
-        }
-
-        data_type dflt;
-        ul size = 1;
-        std::vector<data_type> tree; // for convenience purposes a root is stores at index 1
-        tree_builder_fn fn;
     };
 }
 
